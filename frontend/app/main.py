38a30f45cc18c838d6d92bb7e0bb236ba32d5190
Costa Constantinou <costa365@gmail.com>
2023-04-09 16:46:57 +0300

    Added frontend doc post

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -3,6 +3,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import requests
+import app.schemas as schemas
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -13,8 +14,16 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/doc/{id}", response_class=HTMLResponse)
-async def root(request: Request, id:str):
+async def get_doc(request: Request, id:str):
     r = requests.get(url='http://backend:8000/doc/'+id)
     j = r.json()
     m = j['doc']
     return templates.TemplateResponse("doc.html", {"request": request, "markdown":m})
+
+
+@app.post("/doc")
+async def create_doc(md:schemas.MarkDown):
+    body = {'doc':md.doc}
+    resp = requests.post(url='http://backend:8000/doc/', json=body)
+    js = resp.json()
+    return {'doc': str(js['doc'])}
---
+++ frontend/app/schemas.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class MarkDown(BaseModel):
+    doc: str
